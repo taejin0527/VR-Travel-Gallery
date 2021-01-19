d664e7b314fde6b06d29183d4acf6ca41a0224e9
taewan <fksk94@gmail.com>
2021-01-19 23:21:05 +0900

    [FrontEnd] feat/main: 렌딩페이지 및 메인 월드맵 페이지

---
+++ frontend/src/views/WorldContinents/EachContinent.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-main>
+    <h1>대륙별 페이지입니다.</h1>
+    <h1>유럽페이지만 만들어 볼 예정입니다.</h1>
+    <h1>모든 페이지를 만들기엔 시간이 좀 많이 걸릴 것 같더라구요.</h1>
+    <h1>노가다 같이 합시다 여러분 ^^</h1>
+    <h1>아니면 디비에 각 나라이름과 사진을 담을 수 있다면 좋은데</h1>
+    <h1>이러면 로딩시간이 너무 길어지지 않을까 걱정입니다.</h1>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: 'EachContinent',
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
