88d7c963dc7d1a1d52887e0327ee582e1960b501
taewan <fksk94@gmail.com>
2021-01-28 21:57:48 +0900

    [FrontEnd] refactor: 이미지 및 컴포넌트 경로 @으로 변경 및 폴더명 캐멀표시

--- frontend/src/components/Continents/NorthAmerica.vue
+++ frontend/src/components/Continents/NorthAmerica.vue
@@ -8,7 +8,7 @@
       >
         <div class="d-flex justify-center mb-6">
           <img
-            src="../../assets/north_america.png"
+            src="@/assets/continents/north_america.png"
             alt="image error"
             height = "600px"
           >
