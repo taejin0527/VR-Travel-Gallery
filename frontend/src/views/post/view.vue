3ac6e74593c4aeaca2b5420d0e29ca95e285b535
donggeolKim <ehd6481@naver.com>
2021-01-29 12:49:44 +0900

    [back] proto1

--- frontend/src/views/post/view.vue
+++ frontend/src/views/post/view.vue
@@ -15,13 +15,7 @@ export default {
     },
     created:function(){
          
-         axios.get('http://localhost:8080/board/getposts',
-            {
-              headers:{
-                      'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzc2FmeSIsImlhdCI6MTYxMTgzNTY2MCwiZXhwIjoxNjExOTIyMDYwfQ.Fx55z-74k_YM1S-MhGEHeIXNSkCWvzRO25DYvOKSjFxhBl3iY7_3mSnj_a8Yokur8dSJpLSi92H-1oij4bP5MA'
-              
-                  }
-            }).then(response => {
+         axios.get('http://localhost:8080/board/getposts').then(response => {
                 this.files = response.data;
                 console.log(this.files[0].filename);
         }).catch(function(){
