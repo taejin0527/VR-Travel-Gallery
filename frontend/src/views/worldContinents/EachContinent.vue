9cc0a4e99572e60d5950ef477353866e2e67272e
taewan <fksk94@gmail.com>
2021-02-01 15:14:01 +0900

    [FrontEnd] feat/cardview: 각 위치마다 카드뷰 생성

--- frontend/src/views/worldContinents/EachContinent.vue
+++ frontend/src/views/worldContinents/EachContinent.vue
@@ -1,5 +1,17 @@
 <template>
   <v-main>
+    <v-btn
+      elevation="3"
+      fab
+      color="#DDA288"
+      style="position:fixed; right:20px; top:20px; color:white;"
+      @click="clickChangeContinentViewButton"
+    >
+      <span v-if="popularExhibition"> ALL </span>
+      <v-icon v-else>
+        mdi-star
+      </v-icon>
+    </v-btn>
     <!-- 각 대륙별로 이미지 가져오기 -->
     <div v-if="this.getContinentName == 'oceania'">
       <Oceania/>
@@ -34,7 +46,8 @@ export default {
   name: 'EachContinent',
   data: function () {
     return {
-      "getContinentName": localStorage.getItem('continent')
+      "getContinentName": localStorage.getItem('continent'),
+      "popularExhibition": true,
     }
   },
   components: {
@@ -44,6 +57,11 @@ export default {
     Asia,
     Africa,
     Europe
+  },
+  methods: {
+    clickChangeContinentViewButton: function () {
+      this.popularExhibition = !this.popularExhibition
+    }
   }
 
 }
