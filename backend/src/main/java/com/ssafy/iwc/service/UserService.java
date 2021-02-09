a4c3b982c9639ce8a7f0ee6cda3bf1160dc6239b
donggeolKim <ehd6481@naver.com>
2021-02-09 14:46:36 +0900

    aa

--- backend/src/main/java/com/ssafy/iwc/service/UserService.java
+++ backend/src/main/java/com/ssafy/iwc/service/UserService.java
@@ -38,5 +38,5 @@ public interface UserService {
 
 	public boolean send(String subject, String text, String to);
 
-	public void updateUserMoney(Integer total);
+	public void updateUserMoney(Integer total,String userid);
 }
