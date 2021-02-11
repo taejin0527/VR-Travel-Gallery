c92f7d96525bcd7e8e312600037cdd1cc98717dc
donggeolKim <ehd6481@naver.com>
2021-02-11 19:22:51 +0900

    S04P13D110-163 좋아요 처리완료

--- backend/src/main/java/com/ssafy/iwc/repository/BoardRepository.java
+++ backend/src/main/java/com/ssafy/iwc/repository/BoardRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.iwc.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -16,4 +17,11 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
 	
 	@Query(value = "select * from posts where location = ? order by id desc limit ?,?",nativeQuery = true)
 	List<Board> getLocationIdxBoard(String location, int start, int idx);
+
+	@Query(value = "select good from posts where id = ? ",nativeQuery = true)
+	Long countSu(long id);
+
+	@Modifying
+	@Query(value = "update posts set good = ? where id = ? ",nativeQuery = true)
+	void fixLikes(Long likes, Long id);
 }
--- backend/src/main/java/com/ssafy/iwc/repository/LikeRepository.java
+++ backend/src/main/java/com/ssafy/iwc/repository/LikeRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.iwc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +13,17 @@ public interface LikeRepository extends JpaRepository<Like, MultiId>{
 
 	@Query(value = "select count(postsid) from likes where username = ? and postsid = ?",nativeQuery = true)
 	int findLike(String username, long postsId);
+
+	@Modifying
+	@Query(value = "delete from likes where postsid = ? and username = ? ",nativeQuery = true)
+	void deleteLike(long id, String username);
+
+	@Modifying
+	@Query(value = "insert into likes values (?,?) ",nativeQuery = true)
+	void addLike(long id, String username);
+	
+	@Modifying
+	@Query(value = "delete from likes where postsid = ? ",nativeQuery = true)
+	void deleteByNo(Long no);
 
 }
