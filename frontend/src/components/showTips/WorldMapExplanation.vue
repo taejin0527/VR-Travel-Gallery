876f61cc519318bb55a72afbda53b4b76cf8890e
taewan <fksk94@gmail.com>
2021-02-14 03:26:10 +0900

    [FrontEnd] fix/main : 메인 페이지 디자인 개선

---
+++ frontend/src/components/showTips/WorldMapExplanation.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    style="color:white; font-size:20px"
+  >
+    <div
+      class="d-flex justify-start"
+    >
+      왼쪽 버튼을 통해 대륙별 여행사진을
+    </div>
+    <div
+      class="d-flex justify-center"
+    >
+      
+    </div>
+    <div
+      class="d-flex justify-start"
+    >
+      감상할 수 있습니다.
+    </div>
+    <br>
+    <div
+      class="d-flex justify-start"
+      style="font-size:15px;"
+    >
+      <pre> </pre> ※ 가고 싶은 여행지의 사진을 감상하세요
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"WorldMapExpanation",
+}
+</script>
+
+<style>
+
+</style>
