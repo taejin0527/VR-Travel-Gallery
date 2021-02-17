fcd318a9ff5dca90d31df258a6a3640f335dbaba
taewan <fksk94@gmail.com>
2021-02-18 01:55:43 +0900

    [FrontEnd] feat/author : author getArticle 컴포넌트 만드는 중

---
+++ frontend/src/components/GetUserArticles.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    hi
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+import SERVER from "@/apis/UrlMapper.ts";
+
+
+export default {
+  name:"GetUserArticles",
+  data: function () {
+
+  },
+  methods: {
+    getArticles: function () {
+      axios
+        .get(
+          `${SERVER.BOARD_BASE_URL}paging?location=${locations[index]}&num=${realPage}`
+        )
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
