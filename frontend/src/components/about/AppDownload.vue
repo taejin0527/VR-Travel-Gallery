73d2eda9794c59501686f7239b6c57979577e8a1
taewan <fksk94@gmail.com>
2021-02-18 14:37:18 +0900

    [FrontEnd] fix/connect&design: 북마크 백엔드 연동 및 디자인 그리드 개선

--- frontend/src/components/about/AppDownload.vue
+++ frontend/src/components/about/AppDownload.vue
@@ -2,7 +2,7 @@
   <v-container fulid pa-0 my-10 class="home-intro" id="home">
     <v-container>
       <v-row>
-        <v-col xs="12" sm="12" md="6" cols="12">
+        <v-col xs="12" sm="12" md="6" cols="12" offset-xl="1" xl="5">
           <div class="content">
             <h3><span class="line"></span>WELCOME TO NUVO</h3>
             <h2>Art</h2>
@@ -22,7 +22,7 @@
             </ul>
           </div>
         </v-col>
-        <v-col xs="12" sm="12" md="6" cols="12">
+        <v-col xs="12" sm="12" md="6" cols="12" xl="5">
           <div class="content-image">
             <img src="@/assets/main/NUVO.png" alt="" />
           </div>
